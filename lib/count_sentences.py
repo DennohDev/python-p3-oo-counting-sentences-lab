0d06340f8b61df774177c4d3d87cd02a2292541f
DennohDev <=>
2023-11-30 19:11:07 +0300

    Completed Assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -20,17 +20,8 @@ class MyString:
         return self.value.endswith('!')
 
     def count_sentences(self):
-        # Replace multiple punctuation marks with a single dot
         cleaned_text = re.sub(r'[.?!]+', '.', self.value)
-        # Split the text using dot as the separator
         sentences = cleaned_text.split('.')
-        # Filter out empty sentences (resulting from consecutive dots)
         valid_sentences = [sentence for sentence in sentences if sentence]
         return len(valid_sentences)
 
-# Example usage:
-# string = MyString("This is a string! It has three sentences. Right?")
-# print(string.is_sentence())  # Output: True
-# print(string.is_question())  # Output: False
-# print(string.is_exclamation())  # Output: True
-# print(string.count_sentences())  # Output: 3
